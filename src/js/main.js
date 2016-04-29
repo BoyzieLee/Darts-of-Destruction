eb0722137b0f789f812ab8c5b2ee917ec18e4214
Boyzie Mathis <boyzielee@gmail.com>
2016-04-29 14:44:28 -0400

    testimonials pictures are on screen

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,31 @@ import $ from 'jquery';
 import carousel from './templates/carousel';
 import testimonials from './templates/testimonials';
 
-var url = 'https://randomuser.me/api/?inc=picture';
+// assign api url to a variable for later use
+var url = 'https://randomuser.me/api/?results=3';
 
-// import mapscroll from './templates/mapscroll';
+  // assign HTML area where template will render profile cards on the page
+  // will append results here later
+  var testimonialsArea = $('.testimonials-row');
 
-// var bootstrap = require('bootstrap');
-// console.log(bootstrap);
+  // Make My Request
+  var dataRequest = $.getJSON(url);
 
-console.log($);
+  var testimonialsHTML = '';
+
+  // When it succedes, call my template Card Function
+  dataRequest.then( function (res) {
+    var formArr = res.results;
+    formArr.forEach( function (user) {
+      console.log(user);
+      // console.log(user);
+      // console.log(user.picture.medium);
+      // passing each user into testimonial function for processing
+      var html = testimonials(user);
+      testimonialsHTML += html;
+
+    });
+
+    // append each result user card to html section class=cardArea
+    testimonialsArea.append(testimonialsHTML);
+  });
--- src/js/templates/testimonials.js
+++ src/js/templates/testimonials.js
@@ -1,52 +1,12 @@
-import $ from 'jquery';
-
-// assign api url to a variable for later use
-var url = 'https://randomuser.me/api/?inc=picture';
-
-
-$.ajax({
-  url: 'https://randomuser.me/api/?inc=picture',
-  dataType: 'json',
-  success: function(data){
-    console.log(data);
-  }
-});
-
-  // assign HTML area where template will render profile cards on the page
-  // will append results here later
-  var cardArea = $('.profiles');
-
-  // Make My Request
-  var dataRequest = $.getJSON(url);
-
-  console.log(dataRequest);
-  var cardsHTML = '';
-
-  // When it succedes, call my template Card Function
-  dataRequest.then( function (res) {
-    var formArr = res.results;
-    formArr.forEach( function (user) {
-      console.log(user);
-      console.log(user.picture.medium);
-
-      // passing each user into picture function for processing
-      var html = testimonials(user);
-      cardsHTML += html;
-    });
-
-    // append each result user card to html section class=cardArea
-    cardArea.append(cardsHTML);
-  });
 
   function testimonials (user) {
     return `
     <div class="col-md-4 text-center">
-      <img src="https://randomuser.me/api/portraits/med/men/66.jpg" alt="" />
+      <img src="${user.picture.medium}" alt="" />
       <h5>FirsName LastName</h5>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
     </div>
     `;
   }
 
-
-export default testimonials
+export default testimonials;
