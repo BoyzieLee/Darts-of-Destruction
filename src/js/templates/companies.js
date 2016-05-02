66624212dd57bb846a6d7922931509461f731b2d
Boyzie Mathis <boyzielee@gmail.com>
2016-05-02 06:38:48 -0400

    added companies code

--- src/js/templates/companies.js
+++ src/js/templates/companies.js
@@ -1,5 +1,24 @@
+import $ from 'jquery';
 
+var companies = $('.companies');
+// console.log(companies);
 
+function companyPics (companies) {
+  return `
+    <img src="${results.image_url}">
+    <img src="${results.image_url}">
+    <img src="${results.image_url}">
+    <img src="${results.image_url}">
+  `;
+}
+var resultsRequest = $.getJSON('https://json-data.herokuapp.com/darts/companies');
 
+resultsRequest.then( function (res) {
+  res.results.forEach(function(companies) {
+    // console.log(companies);
+    var html = companyPics(res);
+    companies.append(html);
+  });
+});
 
-export default compnaies;
+export default companies;
